8a584f5577306d82ca5b82399948ceaba619267d
NeelaCharan <karthikeyasarma186@gmail.com>
2020-03-18 20:06:20 +0530

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes , RouterModule} from '@angular/router';
+import { StudentModule} from './student/student.module';
+import { StudentComponent} from './student/student/student.component';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {
+      path: '',
+      component: StudentComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StudentModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/student/student.model.ts
@@ -0,0 +1,7 @@
+export class Student {
+  id: number;
+  name: string;
+  enrollno: number;
+  college: string;
+  university: string;
+}
